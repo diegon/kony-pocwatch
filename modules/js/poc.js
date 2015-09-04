6281c692e656a6c6af99947e4c5f78855087d3b9
Diego Negrelli <diegon@gmail.com>
2015-09-04 16:44:55 -0300

    minor refactory

--- modules/js/poc.js
+++ modules/js/poc.js
@@ -1,39 +1,41 @@
 function configureNotifications() {
 
-//Sample code to create actions. Make sure this code is executed before notification gets fired.
-	
-	var ReadMailObj = kony.notificationsettings.createAction({"id":"ReadMail", "label":"ReadMail", "pspConfig":{"activationMode":kony.notificationsettings.ACTIVATION_MODE_FORWARDS, "authenticationRequired": true, "destructive": true, visibleOn: kony.notificationsettings.BOTH}});
-	
+	//Sample code to create actions. Make sure this code is executed before notification gets fired.
+	var ReadMailObj = kony.notificationsettings.createAction({"id":"ReadMail", "label":"ReadMail", "pspConfig":{"activationMode":kony.notificationsettings.ACTIVATION_MODE_FORWARDS, "authenticationRequired": true, "destructive": true, visibleOn: kony.notificationsettings.BOTH}});	
 	var DeleteMailObj = kony.notificationsettings.createAction({"id":"DeleteMail", "label":"DeleteMail", "pspConfig":{"activationMode":kony.notificationsettings.ACTIVATION_MODE_FORWARDS, "authenticationRequired": true, "destructive": true, visibleOn: kony.notificationsettings.BOTH}});
 	
 	
-//Sample code to create category
-	
-	
+	//Sample code to create category
 	var actionArr = [ReadMailObj, DeleteMailObj];
 	var minActionArr = [ReadMailObj, DeleteMailObj];
-	    
 	var categoryObj = kony.notificationsettings.createCategory({"categoryId":"newmail", "actions":actionArr, "pspConfig":{"minimalActions": minActionArr}});
 	
-	
-	
-//Registering Category
-	
+	//Registering Category
 	var categoryArr = [categoryObj];
 	kony.notificationsettings.registerCategory( {"categories": categoryArr,"pspConfig":{ "types":[0,1,2]}} );
 	
+	//Registering callbacks
 	kony.localnotifications.setCallbacks({onlinenotification:online_callback, offlinenotification: offline_callback});	
+}
 
+function online_callback(notificationobject, actionid){
+	kony.print("online_callback");
+	kony.print(actionid);
+	kony.print(notificationobject);
+}
+
+
+function offline_callback(notificationobject, actionid){
+	kony.print("offline_callback");
+	kony.print(actionid);
+	kony.print(notificationobject);
 }
 
 function openNotification() {
 
-//Sample code to create notification.
-
-	var datalocal = new Date();
-	
+	//Sample code to create notification.
 	var notificationId = "01";
-    var date = datalocal.getDate() + " Mar 2015 15:00:00 +0530";
+    var date = "27 Mar 2015 15:00:00 +0530";
     var format = "dd MMM yyyy HH:mm:ss Z";
     var message = "Local notification Received";
     var title = "Title";
@@ -49,17 +51,4 @@ function openNotification() {
         "categoryId": categoryId
     });
 
-}
-
-function online_callback(notificationobject, actionid){
-	kony.print("online_callback");
-	kony.print(actionid);
-	kony.print(notificationobject);
-}
-
-
-function offline_callback(notificationobject, actionid){
-	kony.print("offline_callback");
-	kony.print(actionid);
-	kony.print(notificationobject);
 }
